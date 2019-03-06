e60f35bc293b4ca25f6b6b34bdf593512dc8fda8
M4C3RAVO <chamika.dimantha@gmail.com>
2019-03-06 14:36:39 +0530

    Changed Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'MIND HABIT Online Library system';
+  title = 'I m Mac Bravo';
 }
